16b7eba33d29a910d06166db922317d58fb82b68
jaba claw <nastya.v.krutilina@gmail.com>
2025-09-14 11:03:46 +0300

    added solution for exe num1.2.4

--- firstPacket/num1.2.4.c
+++ firstPacket/num1.2.4.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+int main() {
+    int n, a, i = 0;
+    float sum = 0.0, average;
+    scanf("%d",&n);
+    for (i = 0; i < n; i++) {
+        scanf("%d",&a);
+        sum += a;
+    }
+    average = sum/n;
+    printf("%.2f",average);
     return 0;
 }
