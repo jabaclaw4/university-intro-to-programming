035f6b77d2253786e5b4b3a22957947c1d49805e
jaba claw <nastya.v.krutilina@gmail.com>
2025-09-23 12:40:44 +0300

    added solution for exercise num1.2.43

--- firstPacket/num1.2.43.c
+++ firstPacket/num1.2.43.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    int n, a, count = 0, prev, next, sise;
-    scanf("%d", &sise);
+    int n, a, count = 1, prev, next, i = 0; //count длина участка i кол во подходящих участков
+    scanf("%d", &n);
+    scanf("%d", &prev);
     while (scanf("%d", &next) == 1) {
-
+        if (prev == next) {
+            count++;
+        }
+        else {
+            if (count >= n) {
+                i++;
+            }
+            count = 1;
+            prev = next;
+        }
     }
-    printf("%d",count);
+    printf("%d", i);
     return 0;
 }
