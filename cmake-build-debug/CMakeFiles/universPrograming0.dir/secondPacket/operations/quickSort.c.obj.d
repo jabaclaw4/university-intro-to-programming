097821513eb3c5c91031bbd7a6b70d996e7e1ea4
anastasiia <nastya.v.krutilina@gmail.com>
2025-09-30 23:00:26 +0300

    added time and random digits

---
+++ cmake-build-debug/CMakeFiles/universPrograming0.dir/secondPacket/operations/quickSort.c.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/universPrograming0.dir/secondPacket/operations/quickSort.c.obj: \
+ C:\Users\User\CLionProjects\university-intro-to-programming\secondPacket\operations\quickSort.c \
+ C:/Users/User/CLionProjects/university-intro-to-programming/secondPacket/headerFiles/quickSort.h
--- secondPacket/main.c
+++ secondPacket/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "headerFiles/array_io.h"
 #include "headerFiles/num1.3.10.h"
 #include "headerFiles/num1.3.15.h"
@@ -6,32 +7,31 @@
 #include "headerFiles/num1.3.27.h"
 #include "headerFiles/bubbleSort.h"
 #include "headerFiles/quickSort.h"
+#include <time.h>
 
 int main() {
-    const int n = 6;
+    clock_t start, end;
+    double cpu_time_used;
+    start = clock();
+
+    const int n = 15;
     int a[n], b[n], c[n];
-    int answer, len, i;
-//    scanf("%d",&len); для последней задачи
-    inputArray(a, n);
-//    inputArray(b, n); для 1.3.15
-
-//    printf("first array up to: "); для 1.3.10
-//    printArray(a, n);
-//    calculateHalfSums(a, n);
-
-//    intersection(a,b,n,c); для 1.3.15
-//    answer = intersection(a,b,n,c);
-//    printf("result: ");
-//    printArray(c, n);
-//    printf("\nanswer: ");
-//    printf("%d",answer);
-
-//    deleteDuplicates(a,n); для 1.3.19
-//    printArray(a,n);
-
-//    for (i = 0; i <= len; i++)
-//        printf("%d ",binomK(len,i)); для последней задачи
-    quick(a,0,n-1);
+//    int answer, len, i;
+//    scanf("%d",&len); для 1.3.27
+    inputRandArray(a, n);
     printArray(a,n);
+//    calculateHalfSums(a, n); для 1.3.10
+//    intersection(a,b,n,c); для 1.3.15
+//    deleteDuplicates(a,n); для 1.3.19
+//    for (i = 0; i <= len; i++)
+//        printf("%d ",binomK(len,i)); для 1.3.27
+    quick(a,0,n-1);
+    printf("%d elements of the array (after): ", n);
+    printArray(a,n);
+
+    end = clock();
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("program works %.6f sec\n", cpu_time_used);
+
     return 0;
 }
