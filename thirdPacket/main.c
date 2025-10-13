5e65b0519b6913811676e71c2f5c21936eb24755
anastasiia <nastya.v.krutilina@gmail.com>
2025-10-13 13:43:11 +0300

    fixed

--- thirdPacket/main.c
+++ thirdPacket/main.c
@@ -1,11 +1,16 @@
 #include "headerFiles/arrayIO.h"
 #include "stdlib.h"
 #include "headerFiles/reverse.h"
+#include "stdio.h"
 
 int main() {
     int n = getArraySize();
     int *arr = NULL;
     arr = (int*)malloc(n * sizeof(int));
+    if (arr == NULL) {
+        printf("error");
+        return 1;
+    }
     inputArray(arr, n);
     reverse(arr, n); // вывод в обратном порядке
     free(arr);
