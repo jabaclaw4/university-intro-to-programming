ced8c592fcc3c89d9c64c0bb1d179916edf1eea2
anastasiia <nastya.v.krutilina@gmail.com>
2025-10-20 22:49:37 +0300

    pc

--- thirdPacket/operations/swapAny.c
+++ thirdPacket/operations/swapAny.c
@@ -1,8 +1,13 @@
 #include "../headerFiles/swapAny.h"
 #include <stdlib.h>
+#include <string.h>
 
 void swapAny(void *a, void *b, size_t size) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+    char temp[size];
+    memcpy(temp, a, size);  // A → temp
+    memcpy(a, b, size);     // B → A
+    memcpy(b, temp, size);  // temp → B
 }
+// в C char всегда имеет размер 1 байт
+// это делает char[] хорошим буфером для побайтовой работы с памятью
+// void* memcpy(void* a, void* b, size_t n) где dest - указатель на куда копировать src - указатель на откуда копировать n - сколько байт скопировать
